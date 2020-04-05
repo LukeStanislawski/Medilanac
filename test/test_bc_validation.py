fbc81b1a1d5e4f9d5ff908416fc89cf4cc0f80f8
psyls6 <psyls6@nottingham.edu.cn>
2020-04-05 09:51:32 +0100

    Fixed typo

--- test/test_bc_validation.py
+++ test/test_bc_validation.py
@@ -2,21 +2,21 @@ import sys, os
 import json
 sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
 from miner.miner import Miner
-from utils.validator import validate_headders
+from utils.validator import validate_headers
 from utils.config import Config
 
 
 def test_bc_headers_valid():
-	with open(os.path.join(Config.test_data_dir, "bc_headders_v.json")) as f:
+	with open(os.path.join(Config.test_data_dir, "bc_headers_v.json")) as f:
 		bc = json.loads(f.read())
-	assert len(validate_headders(bc)) == 0, "test failed"
+	assert len(validate_headers(bc)) == 0, "test failed"
 
 
 def test_bc_headers_invalid():
 	for i in range(2):
-		fpath = os.path.join(Config.test_data_dir, "bc_headders_iv_{}.json".format(i))
+		fpath = os.path.join(Config.test_data_dir, "bc_headers_iv_{}.json".format(i))
 		with open(fpath) as f:
 			bc = json.loads(f.read())
-		assert len(validate_headders(bc)) > 0, "test failed"
+		assert len(validate_headers(bc)) > 0, "test failed"
 
 # test_bc_headers()
